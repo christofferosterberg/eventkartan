45c6b823c1e87a4147bbd3e12f21a5f21b15604c
christoffer <chriistoffer.osterberg@gmail.com>
2024-02-18 21:28:06 +0100

    added admin django view

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import EmailAddress, Company, Event, SubscriptionOption, Subscription
+admin.site.register(EmailAddress)
+admin.site.register(Company)
+admin.site.register(Event)
+admin.site.register(SubscriptionOption)
+admin.site.register(Subscription)
